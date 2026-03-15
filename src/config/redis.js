f6f38ace51437ecba5ba01288c9ad74237b23354
aashish <aashesh.j@gmail.com>
2026-03-15 17:21:16 +0530

    docker file2

--- src/config/redis.js
+++ src/config/redis.js
@@ -2,7 +2,12 @@
 import { createClient } from "redis";
 
 export const redis = createClient({
-    url: 'redis://127.0.0.1:6379'
+    username: 'default',
+    password: process.env.REDIS_PASSWORD,
+    socket: {
+        host: 'redis-16092.crce283.ap-south-1-2.ec2.cloud.redislabs.com',
+        port: 16092
+    }
 });
 
 redis.on('error', err => {

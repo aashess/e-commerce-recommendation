f8659d9c8a161da430664764874f72ead74a047d
aashish <aashesh.j@gmail.com>
2026-03-27 22:34:08 +0530

    payment fixing

--- src/controller/payment/razorPayOrder.js
+++ src/controller/payment/razorPayOrder.js
@@ -1,15 +1,19 @@
 import crypto from 'node:crypto';
+import dotenv from 'dotenv'
+dotenv.config();
 import { razorpay } from "../../config/razorpay.js";
 import {
   sendErrorResponse,
   sendSuccessResponse,
 } from "../../utils/responseFormat.js";
-import { error } from 'node:console';
+import { redis } from '../../config/redis.js';
+
 
 
 
 export const createOrder = async (req, res) => {
-  const { amount, currency, notes } = req.body;
+  
+    const { amount, currency, notes } = req.body;
 
   try {
     if (!amount && !currency && !notes) {
@@ -23,27 +27,47 @@ export const createOrder = async (req, res) => {
       notes: { task: notes },
     };
     const paymentResponse = await razorpay.orders.create(options);
-    console.log("Payment Response", paymentResponse);
-    sendSuccessResponse(res, true, 200, "Payment Successful", paymentResponse);
+    redis.set(req.user.id, paymentResponse.id, {EX:  '300'})
+
+    
+    sendSuccessResponse(res, true, 200, "Order Created", paymentResponse);
   } catch (error) {
     console.error(error);
+    sendErrorResponse(res, false, 400, "Order Failed", error)
   }
 };
 
 
 export const verifyPayment = async (req, res) => {
-    const {razorpay_order_id, razorpay_payment_id, razorpay_signature} = req.body
+    try {
+        console.log("Request Reached here://", req.body);
 
-
-    
-    const generatedSignature = crypto.createHmac('sha56', process.env.RAZORPAY_SECRET)
-                .update(razorpay_order_id + "|" + razorpay_payment_id)
-                .digest('hex');
-    
-    if (generatedSignature === razorpay_signature) {
-        sendSuccessResponse(res, true, 200, "Payment Successful")
-    } else {
-        console.error(error);
+        const {razorpay_payment_id, razorpay_signature} = req.body
+     
+        // const razorpay_order_id = order_id
+        const razorpay_order_id = await redis.get(req.user.id)
         
+        console.log("RozorPay_Order_ID: ",razorpay_order_id);
+
+//         console.log("FROM FRONTEND:", req.body.razorpay_order_id);
+// console.log("FROM REDIS:", razorpay_order_id);
+// console.log("MATCH:", req.body.razorpay_order_id === razorpay_order_id);
+        
+        const generatedSignature =  crypto
+                    .createHmac("sha256", '1eAMRglwpZr8VhVT0P7dmMn3')
+                    .update(razorpay_order_id + "|" + razorpay_payment_id)
+                    .digest("hex");
+        console.log("Signature: ", generatedSignature);
+        
+        
+        if (generatedSignature === razorpay_signature) {
+            sendSuccessResponse(res, true, 200, "Payment Successful")
+        } else {
+            console.log("Signature doesn't matched.")
+            sendErrorResponse(res, false, 400, "Signature Doesn't Matched.")
+            
+        }
+    } catch (error) {
+        console.error("Something went wrong at server.", error);
     }
 }
--- src/router/payment/razorpayRoutes.js
+++ src/router/payment/razorpayRoutes.js
@@ -1,9 +1,10 @@
 import { createOrder, verifyPayment } from "../../controller/payment/razorPayOrder.js";
 import { Router } from "express";
+import { authenticateUser } from "../../middleware/auth.middleware.js";
 const router = Router()
 
-router.post('/order', createOrder)
-router.post('verify-payment', verifyPayment)
+router.post('/order',authenticateUser, createOrder)
+router.post('/verify',authenticateUser, verifyPayment)
 
 export default router;
 
